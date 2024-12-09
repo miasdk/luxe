f15af542bbd08fb1380976e04b98624f09d6f4b1
Mia <miaskyelena@gmail.com>
2024-12-09 15:35:22 -0500

    Project Init

---
+++ server/config/reset.js
@@ -0,0 +1,51 @@
+import { pool } from './database.js';
+import './dotenv.js';
+import productData from '../data/products.js';
+
+const createProductsTable = async () => {
+    const createTableQuery = `
+        DROP TABLE IF EXISTS products;
+
+        CREATE TABLE IF NOT EXISTS products ( 
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            description TEXT NOT NULL,
+            price DECIMAL(10, 2) NOT NULL,
+            image_url TEXT NOT NULL
+        )
+    `
+    
+    try {
+        const res = await pool.query(createTableQuery);
+        console.log('Table created successfully');
+    } catch (err) { 
+        console.log('⚠️ error creating gifts table', err);
+    }
+}
+
+const seedProductsTable = async () => { 
+    await createProductsTable();
+
+    productData.forEach((product) => {
+        const insertQuery = {
+            text: 'INSERT INTO products (name, description, price, image_url) VALUES ($1, $2, $3, $4)',
+        }
+
+        const values = [
+            product.title,
+            product.description,
+            product.price,
+            product.image
+        ]
+
+        pool.query(insertQuery, values, (err, res) => {
+            if (err) {
+                console.log('⚠️ error seeding products table', err);
+                return
+            }
+            console.log(`🏬 ${product.name} added successfully`);
+        })
+    })
+}
+
+seedProductsTable();
---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import productsRouter from './routes/products.js';
+import './config/dotenv.js';    
+import cors from 'cors';
+
+
+const app = express(); 
+
+app.use(cors());
+app.use('/products', productsRouter); 
+
+app.get('/', (req, res) => {
+    res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">eCart API</h1>')
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
+
