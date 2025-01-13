e95b7ba75deca14b813bc4d2e30a97acc7d7d35f
Mia <miaskyelena@gmail.com>
2025-01-12 23:07:52 -0500

    Continuation of last commit

--- server/data/categories.js
+++ server/data/categories.js
@@ -1,31 +1,25 @@
 export const categoryData = [
     {
-        id: 1,
         name: 'Dresses',
         image: 'https://media.thereformation.com/image/upload/f_auto,q_auto,dpr_2.0/PRD-SFCC/1317091/NAVY/1317091.1.NAVY?_s=RAABAB0',
     },
     {
-        id: 2,
         name: 'Tops',
         image: 'https://media.thereformation.com/image/upload/f_auto,q_auto,dpr_2.0/PRD-SFCC/1309586/BABYGIRL/1309586.1.BABYGIRL?_s=RAABAB0',
     },
     {
-        id: 3,
         name: 'Sweaters',
         image: 'https://media.thereformation.com/image/upload/f_auto,q_auto,dpr_2.0/PRD-SFCC/1309791/OATMEAL/1309791.1.OATMEAL?_s=RAABAB0',
     },
     {
-        id: 4,
         name: 'Pants',
         image: 'https://media.thereformation.com/image/upload/f_auto,q_auto,dpr_2.0/PRD-SFCC/1314838/PITCH_BLACK/1314838.1.PITCH_BLACK?_s=RAABAB0',
     },
     {
-        id: 5,
         name: 'Skirts',
         image: 'https://media.thereformation.com/image/upload/f_auto,q_auto,dpr_2.0/PRD-SFCC/1315328/BLACK/1315328.1.BLACK?_s=RAABAB0',
     },
     {
-        id: 6,
         name: 'Shoes',
         image: 'https://media.thereformation.com/image/upload/f_auto,q_auto,dpr_2.0/PRD-SFCC/1316234/BLACK_LEATHER/1316234.1.BLACK_LEATHER?_s=RAABAB0',
     },
