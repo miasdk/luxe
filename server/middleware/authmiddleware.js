de1d4d4b51c8b5edebf486999a2c7144167a2b03
Mia <miaskyelena@gmail.com>
2025-04-07 14:56:22 -0400

    Feature/Completed OrderModel

---
+++ server/middleware/authmiddleware.js
@@ -0,0 +1,27 @@
+import admin from '../config/firebaseAdmin.js';
+
+verifyFirebaseToken = async (req, res, next) => {
+    const token = req.headers.authorization?.split('Bearer ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized: No token provided' });
+    }
+
+    try {
+        const decodedToken = await admin.auth().verifyIdToken(token);
+        req.user = {
+            uid: decodedToken.uid,
+            email: decodedToken.email,
+            displayName: decodedToken.name || decodedToken.email || null,
+            photoURL: decodedToken.picture || null,
+        }; 
+        next();
+    } catch (error) {
+        console.error('Error verifying token:', error);
+        return res.status(403).json({ message: 'Unauthorized: Invalid token' });        
+    }
+}
+
+export default verifyFirebaseToken; 
+
+
