a4760f24cf4f97cfb7c9679b5de7775925cc4b3c
Mia Elena <miatapiaswe@gmail.com>
2025-07-17 22:22:23 -0400

    Add luxedemo.vercel.app to CORS allowed origins

--- server/server.js
+++ server/server.js
@@ -25,7 +25,8 @@ const corsOptions = {
   origin: [
     'http://localhost:5173',                      
     'https://e-cart-mu-olive.vercel.app',
-    'https://ecartdemo.vercel.app'          
+    'https://ecartdemo.vercel.app',
+    'https://luxedemo.vercel.app'          
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
